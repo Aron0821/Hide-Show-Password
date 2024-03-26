0132bce2f32d0681afbad49b91066cf932201405
Aron0821 <aronshakha101@gmail.com>
2024-03-26 17:14:53 +0545

    Complete

---
+++ password.js
@@ -0,0 +1,13 @@
+let eyeicon = document.querySelector("#eyeicons");
+let password = document.querySelector("#password");
+
+eyeicon.onclick = function(){
+    if(password.type === "password"){
+        password.type = "text"
+        eyeicon.src = "images/eye-icons/eye-open.png"
+    } else {
+        password.type = "password"
+        eyeicon.src = "images/eye-icons/eye-close.png"
+
+    }
+}
